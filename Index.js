22ba8d7ada3c2b08924d175e95cb84337239649f
Joey Morgan <you@example.com>
2021-11-20 22:05:37 -0600

    week 1

---
+++ Index.js
@@ -0,0 +1,25 @@
+var bankAccountBalance = 50.00; 
+
+var firstName ='Joey';
+var lastName = 'Morgan';
+
+var numberOfFriends = 5;
+
+var isHotOutside = false;
+var isHappy = true;
+
+console.log("hello world");
+
+console.log(5 + 3);
+console.log(4-2);
+console.log(6 * 7);
+console.log(8 / 2);
+
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends);
+
+console.log(5 == 5);
+console.log(bankAccountBalance == numberOfFriends);
+console.log(5 == 6);
+console.log(firstName == bankAccountBalance)
+console.log(3 == 3);
